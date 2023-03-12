b067f83b5d59525784ad7caa1ee2ab3debe1002b
HongBi Kim <hongbikim14@gmail.com>
2023-03-12 20:21:09 +0900

    몇가지 함수계산

--- steps/step24.py
+++ steps/step24.py
@@ -1,7 +1,27 @@
 import numpy as np
 
-import dezero as dz
+from dezero import Variable
 
 
 def sphere(x, y):
-    
+    z = x**2 + y**2
+    return z
+
+
+x = Variable(np.array(1.0))
+y = Variable(np.array(1.0))
+z = sphere(x, y)
+z.backward()
+print(x.grad, y.grad)
+
+
+def matyas(x, y):
+    z = 0.26 * (x**2 + y**2) - 0.48 * x * y
+    return z
+
+
+x = Variable(np.array(1.0))
+y = Variable(np.array(1.0))
+z = matyas(x, y)
+z.backward()
+print(x.grad, y.grad)
