99494d878088c5348e06fc6376d3e6e050856c2e
HongBi Kim <hongbikim14@gmail.com>
2023-03-15 18:09:31 +0900

    고차미분 구현 및 create_graph 부분 구현

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -1,5 +1,5 @@
 # ===================================================================
-is_simple_core = True  # True
+is_simple_core = False  # True
 # ===================================================================
 
 if is_simple_core:
@@ -13,16 +13,16 @@ if is_simple_core:
         using_config,
     )
 
-# else:
-#     from dezero.core import Variable
-#     # from dezero.core import Parameter
-#     from dezero.core import Function
-#     from dezero.core import using_config
-#     from dezero.core import no_grad
-#     # from dezero.core import test_mode
-#     from dezero.core import as_array
-#     from dezero.core import as_variable
-#     from dezero.core import setup_variable
-#     from dezero.core import Config
+else:
+    from dezero.core import Variable
+    # from dezero.core import Parameter
+    from dezero.core import Function
+    from dezero.core import using_config
+    from dezero.core import no_grad
+    # from dezero.core import test_mode
+    from dezero.core import as_array
+    from dezero.core import as_variable
+    from dezero.core import setup_variable
+    from dezero.core import Config
 
 setup_variable()
